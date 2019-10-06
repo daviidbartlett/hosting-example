b449f14de44c86ea14da3086c6225b09d60303dc
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2019-10-06 18:30:21 +0100

    create houses migration up function

--- db/migrations/20191006182651_create_houses_table.js
+++ db/migrations/20191006182651_create_houses_table.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('houses', houses_table => {
+    houses_table.increments('house_id').primary();
+    houses_table
+      .string('house_name')
+      .notNullable()
+      .unique();
+    houses_table.string('founder').notNullable();
+    houses_table.string('animal').notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
