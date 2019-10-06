3e1626401d32e9999dacb1f6667b8a5d06242351
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2019-10-06 18:45:32 +0100

    complete createRef util

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,4 +1,33 @@
 const { expect } = require('chai');
 const { createRef } = require('../utils/');
 
-describe('createRef()', () => {});
+describe('createRef()', () => {
+  it('given an empty array will return an empty object', () => {
+    expect(createRef([], {})).to.eql({});
+  });
+  it('given an array with 1 item will place value of passed key in returned object', () => {
+    const inputArray = [{ name: 'test_name' }];
+    expect(
+      createRef(inputArray, { key: 'name', value: '' })
+    ).to.haveOwnProperty('test_name');
+  });
+  it('given 1 item will place value of value argument in the returned object ', () => {
+    const inputArray = [{ name: 'test_name', id: 1 }];
+    expect(createRef(inputArray, { key: 'name', value: 'id' })).to.eql({
+      test_name: 1
+    });
+    expect(createRef(inputArray, { key: 'id', value: 'name' })).to.eql({
+      1: 'test_name'
+    });
+  });
+  it('will create reference for multiple items in array', () => {
+    const inputArray = [
+      { name: 'test_name', id: 1 },
+      { name: 'another_test', id: 25 }
+    ];
+    expect(createRef(inputArray, { key: 'name', value: 'id' })).to.eql({
+      test_name: 1,
+      another_test: 25
+    });
+  });
+});
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,7 @@
-exports.createRef = (list, key, value) => {
-
-}
+exports.createRef = (list, { key, value }) => {
+  if (!list.length) return {};
+  return list.reduce((ref, item) => {
+    ref[item[key]] = item[value];
+    return ref;
+  }, {});
+};
