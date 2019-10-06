3062942e8bcc23e26ce3c4814f6b2df903e3e59b
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2019-10-06 18:34:55 +0100

    seed houses data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,12 @@
 const { houseData, wizardData } = require('../data/index');
 
 exports.seed = knex => {
-  return knex.migrate.rollback().then(() => knex.migrate.latest());
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      return knex('houses')
+        .insert(houseData)
+        .returning('*');
+    });
 };
