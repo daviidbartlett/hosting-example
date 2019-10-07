f1739e21fbf9c42dc7627c30d0200b7d366456e2
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2019-10-07 11:16:59 +0100

    add basic error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api-router');
-
+const { handleCustomErrors } = require('./errors/index');
 app.use('/api', apiRouter);
+
+app.use(handleCustomErrors);
 module.exports = app;
---
+++ errors/index.js
@@ -0,0 +1,4 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: 'internal server error' });
+};
